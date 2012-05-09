87762b4881addeec7169ffda5b2c520f87d4a3bf
Erich Egert <erich.egert@caelum.com.br>
2012-05-09 11:52:04 -0300

    scaffold do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copas::Application.config.secret_token = '8a5db40368e8ee4d0ae0f45b6a4c9c732aaedd92d6fba8b820c2e07ec86ce85e710c6f0f6fc7d28789add6da7dadbca45efd0e7c38f5db2e8b42e17fdb6bb800'
